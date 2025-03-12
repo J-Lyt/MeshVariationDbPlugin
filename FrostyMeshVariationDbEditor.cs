5c52c2e494ffb754bb558ce8c088bf4f3eae304a
Joel <52287493+J-Lyt@users.noreply.github.com>
2025-03-12 06:42:13 +0000

    Replace Shaders (Material)
    - Clean-up

--- FrostyMeshVariationDbEditor.cs
+++ FrostyMeshVariationDbEditor.cs
@@ -42,7 +42,8 @@ namespace MeshVariationDbPlugin
             toolbarItems.Add(new ToolbarItem("Export", "Export Database", "FrostyEditor;component/Images/Export.png", new RelayCommand((object state) => { ExportButton_Click(this, new RoutedEventArgs()); })));
             toolbarItems.Add(new ToolbarItem("Import", "Import Database", "FrostyEditor;component/Images/Import.png", new RelayCommand((object state) => { ImportButton_Click(this, new RoutedEventArgs()); })));
             toolbarItems.Add(new ToolbarItem("Replace Shaders (All)", null, "MeshVariationDbPlugin;component/Images/Alpha.png", new RelayCommand((object state) => { AlphaButton_Click(this, new RoutedEventArgs()); })));
-            toolbarItems.Add(new ToolbarItem("Replace Shaders (Mesh)", null, "MeshVariationDbPlugin;component/Images/AlphaMesh.png", new RelayCommand((object state) => { AlphaMeshButton_Click(this, new RoutedEventArgs()); })));
+            toolbarItems.Add(new ToolbarItem("Replace Shaders (Mesh)", null, "FrostyEditor;component/Images/Mesh.png", new RelayCommand((object state) => { AlphaMeshButton_Click(this, new RoutedEventArgs()); })));
+            toolbarItems.Add(new ToolbarItem("Replace Shaders (Material)", null, "FrostyEditor;component/Images/Grid.png", new RelayCommand((object state) => { AlphaMatButton_Click(this, new RoutedEventArgs()); })));
             toolbarItems.Add(new ToolbarItem("Find Namehash", null, "MeshVariationDbPlugin;component/Images/Hash.png", new RelayCommand((object state) => { FindHashButton_Click(this, new RoutedEventArgs()); })));
             
             return toolbarItems;
@@ -143,7 +144,7 @@ namespace MeshVariationDbPlugin
 
             if (isAvailable == true)
             {
-                MessageBoxResult result = FrostyMessageBox.Show("Do you wish to replace shaders?", "Replace Shaders", MessageBoxButton.YesNo);
+                MessageBoxResult result = FrostyMessageBox.Show("Do you wish to replace all shaders?", "Replace Shaders", MessageBoxButton.YesNo);
                 if (result == MessageBoxResult.Yes)
                 {
                     App.AssetManager.ModifyEbx(entry.Name, asset);
@@ -282,6 +283,11 @@ namespace MeshVariationDbPlugin
                             }
                         }
                     }
+                    else if (selectedAssetEntry.Filename.EndsWith("rgz_mesh") == false)
+                    {
+                        FrostyMessageBox.Show("'" + selectedAssetEntry.Filename + "' is not a SkinnedMeshAsset.", "Replace Shaders");
+                        return;
+                    }
                 }
             }
 
@@ -390,6 +396,171 @@ namespace MeshVariationDbPlugin
             }
         }
 
+        private void AlphaMatButton_Click(object sender, RoutedEventArgs e)
+        {
+            EbxAssetEntry entry = (EbxAssetEntry)AssetEntry;
+
+            dynamic varEntriesPre = App.AssetManager.GetEbx(entry).RootObject;
+            dynamic varEntryPre = varEntriesPre.Entries;
+
+            bool isAvailable = false;
+            
+            Guid clipTextGuidPre = System.Guid.Empty;
+
+            int shaderNum = 0;
+            int shaderMasterNum = 0;
+            int shaderDecayNum = 0;
+            int shaderDissolveBBNum = 0;
+            int shaderFrozenNum = 0;
+            int shaderDoTMasterNum = 0;
+            int shaderPetrifyNum = 0;
+
+            string shaderMasterText = "Character_Wardrobe_Master_2Sided_Alpha";
+
+            if (varEntryPre.Count > 0)
+            {
+                for (int i = 0; i < varEntryPre.Count; i++)
+                {
+                    var entriesPre = varEntryPre[i];
+
+                    FrostyPropertyGridItemData pointerRefPreGrid = (FrostyPropertyGridItemData)DataContext;
+
+                    string clipTextPre = Clipboard.GetText();
+                    bool isValid = Guid.TryParse(clipTextPre, out _);
+
+                    Guid pointerRefGuidPre;
+
+                    try
+                    {
+                        if (isValid)
+                        {
+                            clipTextGuidPre = Guid.Parse(clipTextPre);
+                        }
+                        else
+                        {
+                            App.Logger.Log("Clipboard must contain a valid Guid.");
+                            return;
+                        }
+
+                        foreach (var meshVariationMaterialPre in entriesPre.Materials)
+                        {
+                            pointerRefGuidPre = ((PointerRef)meshVariationMaterialPre.Material).External.ClassGuid;
+
+                            if (pointerRefGuidPre == clipTextGuidPre && meshVariationMaterialPre.SurfaceShaderId == 3024908220) //Master
+                            {
+                                isAvailable = true;
+                                break;
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        App.Logger.Log("Clipboard contents are invalid.");
+                        return;
+                    }
+                }
+            }
+
+            if (isAvailable == true)
+            {
+                MessageBoxResult result = FrostyMessageBox.Show("Do you wish to replace shaders assigned to:" + "\n\n" + "[" + clipTextGuidPre.ToString() + "]?", "Replace Shaders", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+                    App.AssetManager.ModifyEbx(entry.Name, asset);
+
+                    dynamic varEntries = App.AssetManager.GetEbx(entry).RootObject;
+                    dynamic varEntry = varEntries.Entries;
+
+                    if (varEntry.Count > 0)
+                    {
+                        for (int i = 0; i < varEntry.Count; i++)
+                        {
+                            var entries = varEntry[i];
+
+                            FrostyPropertyGridItemData pointerRefGrid = (FrostyPropertyGridItemData)DataContext;
+
+                            Guid pointerRefGuid;
+                            Guid clipTextGuid = Guid.Parse(Clipboard.GetText());
+
+                            foreach (var meshVariationMaterial in entries.Materials)
+                            {
+                                pointerRefGuid = ((PointerRef)meshVariationMaterial.Material).External.ClassGuid;
+
+                                if (pointerRefGuid == clipTextGuid && meshVariationMaterial.SurfaceShaderId == 3024908220) //Master
+                                {
+                                    meshVariationMaterial.SurfaceShaderId = 1748236261;
+                                    meshVariationMaterial.SurfaceShaderGuid = Guid.Parse("5c665d88-fe38-4515-b8b1-0713fd6a558f");
+
+                                    shaderNum++;
+                                    shaderMasterNum++;
+                                }
+                                else if (pointerRefGuid == clipTextGuid && meshVariationMaterial.SurfaceShaderId == 3366952955) //Decay
+                                {
+                                    meshVariationMaterial.SurfaceShaderId = 2989973410;
+                                    meshVariationMaterial.SurfaceShaderGuid = Guid.Parse("91b79ab7-1f6a-4e76-a00f-372d9b4f64f0");
+
+                                    shaderNum++;
+                                    shaderDecayNum++;
+                                }
+                                else if (pointerRefGuid == clipTextGuid && meshVariationMaterial.SurfaceShaderId == 390866619) //DissolveBB
+                                {
+                                    meshVariationMaterial.SurfaceShaderId = 2336327458;
+                                    meshVariationMaterial.SurfaceShaderGuid = Guid.Parse("c247f7f5-2bc3-4f16-8459-c5022b851360");
+
+                                    shaderNum++;
+                                    shaderDissolveBBNum++;
+                                }
+                                else if (pointerRefGuid == clipTextGuid && meshVariationMaterial.SurfaceShaderId == 1571638619) //Frozen
+                                {
+                                    meshVariationMaterial.SurfaceShaderId = 1828819106;
+                                    meshVariationMaterial.SurfaceShaderGuid = Guid.Parse("762d48c1-097b-4a5d-bc28-2bb78d2a17a5");
+
+                                    shaderNum++;
+                                    shaderFrozenNum++;
+                                }
+                                else if (pointerRefGuid == clipTextGuid && meshVariationMaterial.SurfaceShaderId == 2107662139) //DoTMaster
+                                {
+                                    meshVariationMaterial.SurfaceShaderId = 4224110914;
+                                    meshVariationMaterial.SurfaceShaderGuid = Guid.Parse("a9e6496a-5bf7-4fb9-af37-36dfc47f7cce");
+
+                                    shaderNum++;
+                                    shaderDoTMasterNum++;
+                                }
+                                else if (pointerRefGuid == clipTextGuid && meshVariationMaterial.SurfaceShaderId == 1184284987) //Petrify
+                                {
+                                    meshVariationMaterial.SurfaceShaderId = 2534832770;
+                                    meshVariationMaterial.SurfaceShaderGuid = Guid.Parse("974e1887-a9da-4f98-a698-178b0d2e4d23");
+
+                                    shaderNum++;
+                                    shaderPetrifyNum++;
+                                }
+                            }
+                        }
+                    }
+
+                    InvokeOnAssetModified();
+
+                    dataExplorer.RefreshItems();
+                    typeof(MainWindow).InvokeMember("RefreshTabs", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.InvokeMethod, null, Application.Current.MainWindow, []);
+
+                    App.Logger.Log(
+                        shaderNum + " shader(s) assigned to [" + clipTextGuidPre.ToString() + "] have been replaced successfully." + "\n" +
+                        shaderMasterText + ": " + shaderMasterNum + "\n" +
+                        shaderMasterText + "_Decay_VAR: " + shaderDecayNum + "\n" +
+                        shaderMasterText + "_DissolveBB_VAR: " + shaderDissolveBBNum + "\n" +
+                        shaderMasterText + "_Frozen_VAR: " + shaderFrozenNum + "\n" +
+                        shaderMasterText + "_DoTMaster_VAR: " + shaderDoTMasterNum + "\n" +
+                        shaderMasterText + "_Petrify_VAR: " + shaderPetrifyNum);
+                }
+            }
+
+            if (!isAvailable)
+            {
+                FrostyMessageBox.Show("There are no non-alpha shaders assigned to:" + "\n\n" + "[" + clipTextGuidPre.ToString() + "]", "Replace Shaders");
+                return;
+            }
+        }
+
         private void FindHashButton_Click(object sender, RoutedEventArgs e)
         {
             EbxAssetEntry entry = (EbxAssetEntry)AssetEntry;
@@ -416,7 +587,7 @@ namespace MeshVariationDbPlugin
                         }
                         else
                         {
-                            App.Logger.Log("Clipboard contents must be a number.");
+                            App.Logger.Log("Clipboard must contian a valid namehash value.");
                             return;
                         }
 
